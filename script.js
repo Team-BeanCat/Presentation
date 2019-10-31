90b38aefe74126a8390da5e1f773a13981cf8611
Nigecat <nigel.quick@carey.com.au>
2019-10-31 12:03:26 +1100

    Re-wrote design

--- script.js
+++
@@ -1,8 +0,0 @@
-
-function showPage(id) {
-    elements = document.getElementsByClassName("page");
-    for (var i = 0, element; element = elements[i++];) {
-        element.style.display = "block";
-    }
-    document.getElementById(id).style.display = "contents";
-}   
